838537cf9da2d2489ad67edd6654b521f10a2998
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-04-04 12:15:25 +0200

    built search form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header>
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Katrin Kanape</footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
