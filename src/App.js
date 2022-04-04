8726e5b155a1e4ec5c3ef54c2401f345d0dec204
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-04-04 11:36:17 +0200

    created react app and imported bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a href="#" className="btn btn-success">
+        Hello
+      </a>
+    </div>
+  );
+}
+
+export default App;
