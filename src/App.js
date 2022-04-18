41d391b36fd84b339f26a467d2ee1a0536a6b8ae
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-04-18 13:02:43 +0200

    changed default keyword

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <main>
-          <Dictionary defaultKeyword="magic" />
+          <Dictionary defaultKeyword="serendipity" />
         </main>
       </div>
       <footer className="App-footer">
